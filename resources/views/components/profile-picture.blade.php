dd8a5fa193c817e8076107dcb67c0d29eb8f18e9
DamoFD <voshall@yahoo.com>
2023-07-20 15:19:24 -0700

    fixed img error

--- resources/views/components/profile-picture.blade.php
+++ resources/views/components/profile-picture.blade.php
@@ -1,5 +1,5 @@
 @if ($image)
-    <img class="w-6 h-6 mr-3 rounded-full object-cover" src="{{ $image }}"
+    <img class="w-6 h-6 mr-3 rounded-full object-cover" src="{{ $image }}" />
 @else
     <img class="w-6 h-6 mr-3 rounded-full object-cover" src="https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg" />
 @endif
