cd139da7fcc28965adc275bb153cae2f6089ea93
pbandjib <pbandjib@gmail.com>
2023-07-21 16:36:44 -0400

    Sort latest added

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+
+        $posts = Post::latest()->get();
         return view('post.index', [
             'posts' => $posts
         ]);
