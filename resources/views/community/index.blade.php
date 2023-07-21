d4c065414a44d97d4a809d1c3c0a906ba27ddba3
pbandjib <pbandjib@gmail.com>
2023-07-21 18:01:54 -0400

    View "completed" for communities list

--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -5,19 +5,18 @@
         </x-card>
         @foreach ($communities as $community)
             <x-card class="mt-4">
-                    </div>
-                    <div class="w-full">
-                        <a href="{{route('community.show', $community->id)}}">
-                            <h3 class="font-bold mb-3 ">{{$community->community_name}}</h3>
-                        </a>
+                    <div class="w-full flex">
                         <div class="flex items-center text-greyed-text mb-3">
-                            <img class="h-6 w-6 mr-3 rounded-full object-cover" src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+                            <img class="h-12 w-12 mr-3 rounded-full object-cover" src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"/>
 
                         </div>
-                        <p class="mb-3 border-b border-gray-300 pb-3 break-all">{{$community->community_description}}</p>
+                        <div class="ml-3">
+                            <a href="{{route('community.show', $community->id)}}">
+                                <h3 class="font-bold break-all border-b border-gray-300 pb-3">{{$community->community_name}}</h3>
+                            </a>
+                            <p class="mb-3 border-b border-gray-300 p-3 break-all">{{$community->community_description}}</p>
                         </div>
                     </div>
-                </div>
             </x-card>
         @endforeach
     </x-main-layout>
