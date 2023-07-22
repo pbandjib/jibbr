4e08fb2347e591b181fee3d86f31df299a9c13df
pbandjib <pbandjib@gmail.com>
2023-07-22 14:36:20 -0400

    Route and Kernel for checking for Admin

--- resources/views/community/show.blade.php
+++ resources/views/community/show.blade.php
@@ -5,6 +5,14 @@
         <x-card class="mt-4">
             <h1 class="text-3xl font-extrabold">{{$community->community_name}}</h1>
             <p class="text-greyed-text mt-4">{{$community->community_description}}</p>
+            @if (Auth::user() && collect($community->communityAdmin)->contains(function ($admin) {
+               return $admin->user_id == Auth::user()->id;
+
+               }))
+
+                <a href="{{route('community.edit', $community->id)}}">Edit</a>
+                <a href="{{route('community.delete', $community->id)}}">Delete</a>
+            @endif
         </x-card>
         <x-card class=" flex h-10 w-full mt-4 items-center">
             <p>Feed Filters</p>
